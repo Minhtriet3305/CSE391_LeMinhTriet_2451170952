b5229a4afb7e443a3fa5d95bad9a3594d89ae55b
Minhtriet3305 <leminhtriet343@gmail.com>
2026-05-27 12:35:12 +0700

    [SETUP] Initialize React project

---
+++ practical_exercises/session_04_react_basic/projects/portfolio_react/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import User from './userprofile.jsx'
+import Product from './productinfo.jsx'
+import LifecycleDemo from './lifecircledemo.jsx'
+
+
+function GoodCounter() {
+  const [count, setCount] = useState(0);  // ← useState!
+
+  function handleClick() {
+    setCount(count + 1);  // React biết cần re-render!
+  }
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>✅ Counter "tốt" (dùng useState)</h2>
+      <p>Bộ đếm: {count}</p>
+      <button onClick={handleClick}>Tăng (+1)</button>
+      <p style={{ color: "green" }}>
+        ✅ Nhấn nút → Số trên màn hình CẬP NHẬT!
+      </p>
+    </div>
+  );
+}
+
+export default GoodCounter;
